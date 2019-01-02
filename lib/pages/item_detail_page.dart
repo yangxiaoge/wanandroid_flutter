83c6c78c38dfb2079a2b0be21f8c3a8ea90be671
yangjianan <yangjianan@seuic.com>
2019-01-02 18:29:07 +0800

    文章详情页

--- lib/pages/item_detail_page.dart
+++ lib/pages/item_detail_page.dart
@@ -4,18 +4,47 @@ import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 class ItemDetailPage extends StatefulWidget {
   String url;
   String title;
-  ItemDetailPage({Key key, @required String this.url, @required String this.title})
+  ItemDetailPage(
+      {Key key, @required String this.url, @required String this.title})
       : super(key: key);
 
   _ItemDetailPageState createState() => _ItemDetailPageState();
 }
 
 class _ItemDetailPageState extends State<ItemDetailPage> {
+  final flutterWebviewPlugin = new FlutterWebviewPlugin();
+  @override
+  void initState() {
+    super.initState();
+    flutterWebviewPlugin.onScrollYChanged.listen((double offsetY) {
+      // latest offset value in vertical scroll
+      // compare vertical scroll changes here with old value
+      print("offsetY = $offsetY");
+    });
+
+    flutterWebviewPlugin.onScrollXChanged.listen((double offsetX) {
+      // latest offset value in horizontal scroll
+      // compare horizontal scroll changes here with old value
+      print("offsetX = $offsetX");
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      title: Text(widget.title),
-    ),
-    body: Text(widget.url),);
+    return WebviewScaffold(
+      url: widget.url,
+      appBar: new AppBar(
+        title: Text(widget.title),
+      ),
+      withZoom: true,
+      withLocalStorage: true,
+      hidden: true,
+      initialChild: Container(
+        color: Colors.redAccent,
+        child: Center(
+          child: Text('Waiting.....'),
+        ),
+      ),
+    );
   }
 }
